dec4efdd8ae8c15f487c74f15ecbaffaf6e46d10
Ilia Korvigo <ilia.korvigo@gmail.com>
2017-12-08 22:37:04 +0300

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from __future__ import print_function, division
+from distutils.core import setup
+import os
+
+from Cython.Build import cythonize
+
+os.environ['CFLAGS'] = '-O3 -Wall -std=c++11 -stdlib=libc++'
+
+setup(
+    name="annogen",
+    ext_modules=cythonize("annogen/*.pyx",
+                          include_path=["annogen/"],
+                          language="c++"),
+    packages=["annogen"],
+    install_requires=["cython>=0.27"]
+)
